4c8671186841552e4ed9cacfd41e7326e6da7abc
Vero <vero@famille.fr>
2019-06-14 20:08:28 +0200

    Création d'un objet syntaxe (liste des syntaxes et de leur définitions)

--- script/Syntaxe.js
+++ script/Syntaxe.js
@@ -16,8 +16,13 @@ var Syntaxe = {
 		definition: '',
 		theme:'' 
 	},
-	'':{
-		syntaxe: '',
+	'SC.action':{
+		syntaxe: 'SC.action()',
+		definition: "<p>SC.action() est une action atomique au sens des SugarCubes. Cela veut dire qu’elle s’exécute intégralement de manière atomique au cours de l’instant ou l’instruction <em>SC.action<em> est activée par le moteur réactif.<br>SC.action() fait une action externe qui ne fait pas partie de SugarCubes : L’action est une fonction JavaScript.<br>Dans le cadre d’un cube (sorte d’objet réactif), l’action est une méthode de l’objet JavaScript associé.</p>"
+		theme:'' 
+	},
+	'SC.actionOn':{
+		syntaxe: 'SC.actionOn(evt, actionDeclancheSiEvtPresent, actionDeclancheSiEvtAbsant, nbreDinstant)',
 		definition: '',
 		theme:'' 
 	},

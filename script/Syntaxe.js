808d919400eb66ec1e7b02ddd00a55d937c8ef83
Vero <vero@famille.fr>
2019-06-14 15:11:22 +0200

    avancé dans le manuel

---
+++ script/Syntaxe.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Syntaxe = {
+	'SC.machine':{
+		syntaxe: 'SC.machine(int_nbreDeMilisecondesEntreChaqueDemarrageDEtape)',
+		definition: '',
+		theme:'machine sugarCubes' 
+	},
+	'addProgram':{
+		syntaxe: 'addProgram(objetSugarCubes)',
+		definition: '',
+		theme:'ajouter un proramme' 
+	},
+	'':{
+		syntaxe: '',
+		definition: '',
+		theme:'' 
+	},
+	'':{
+		syntaxe: '',
+		definition: '',
+		theme:'' 
+	},
+}
+

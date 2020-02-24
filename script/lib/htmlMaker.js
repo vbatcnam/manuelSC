30b2943f0097edf4718093b2266897a72a92f84a
vbatcnam <vbatcnam@example.net>
2020-02-24 21:29:04 +0100

    quelques améliorations...

--- script/lib/htmlMaker.js
+++ script/lib/htmlMaker.js
@@ -1,12 +1,11 @@
 function createEltHtml (balise, texte){
-	var b = document.createElement(balise);
+	let b = document.createElement(balise);
 	b.innerHTML = texte;
 	return b;
 }
 
 function crateDicoSyntaxes(dico){
-	var liste = document.createElement("dl");
-
+	let liste = document.createElement("dl");
 	for (cle in dico){
 		liste.append(createEltHtml ("dt", cle));
 		liste.append(createEltHtml ("dd", dico[cle]["definition"]));
